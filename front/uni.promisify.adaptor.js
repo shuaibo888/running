8fe9a69a74bc20a72c344c05b42195dde52bf718
cbo <cbo@188.com>
2026-09-03 13:53:47 +0800

    chore: complete initial project scaffold

--- front/uni.promisify.adaptor.js
+++ front/uni.promisify.adaptor.js
@@ -5,7 +5,7 @@ uni.addInterceptor({
     }
     return new Promise((resolve, reject) => {
       res.then((res) => {
-        if (!res) return resolve(res) 
+        if (!res) return resolve(res)
         return res[0] ? reject(res[0]) : resolve(res[1])
       });
     });
